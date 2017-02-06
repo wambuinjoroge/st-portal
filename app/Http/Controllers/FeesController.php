7ab40656dcab572edbb7f30c9ffb0046b3b5cc01
wambuinjoroge <christinenjoroge93@gmail.com>
2017-02-06 12:56:37 -0500

    transactions

--- app/Http/Controllers/FeesController.php
+++ app/Http/Controllers/FeesController.php
@@ -3,32 +3,80 @@
 namespace App\Http\Controllers;
 
 use App\Fees;
+use App\Student;
 use Illuminate\Http\Request;
 
 use App\Http\Requests;
 use Illuminate\Support\Facades\Validator;
+use Symfony\Component\HttpKernel\EventListener\ValidateRequestListener;
 
 class FeesController extends Controller
 {
     //
     public function index(){
-      }
+       $fees=Fees::all();
+       return view('fees.index',compact('fees'));
+    }
     public function create(){
-       }
-
+       $students=Student::all();
+       return redirect('fees.create',compact('students'));
+    }
     public function store(Request $request){
+       $validator=Validator::make($request->all(),[
+           "amount"=>"required",
+           "type"=>"required",
+           "semester"=>"required",
+       ]);
+       if ($validator->fails()){
+           return redirect()->back()
+               ->withErrors($validator)
+               ->withInput();
        }
-    public function show(){
+       $fee=new Fees();
 
+       $fee->amount=$request->get('amount');
+       $fee->type=$request->get('type');
+       $fee->semester=$request->get('semester');
+       $fee->student_id=$request->get('student_id');
+
+       $fee->save();
+
+       return redirect('fees');
+    }
+    public function show($id){
+       $fee=Fees::find($id);
+       return view('fees.show',compact('fee'));
     }
     public function edit($id){
-
+       $fee=Fees::find($id);
+       return view('fees.edit',compact('fee'));
     }
     public function update(Request $request ,$id){
+        $validator=Validator::make($request->all(),[
+            "amount"=>"required",
+            "type"=>"required",
+            "semester"=>"required",
+        ]);
+        if ($validator->fails()){
+            return redirect()->back()
+                ->withErrors($validator)
+                ->withInput();
+        }
+        $fee=Fees::find($id);
 
+        $fee->amount=$request->get('amount');
+        $fee->type=$request->get('type');
+        $fee->semester=$request->get('semester');
+        $fee->student_id=$request->get('student_id');
+
+        $fee->save();
+
+        return redirect('fees');
     }
     public function destroy($id){
-
+        $fee=Fees::find($id);
+        $fee->delete();
+        return redirect('fees');
     }
 
 }
---
+++ database/migrations/2017_02_06_175232_create_transactions_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateTransactionsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('transactions', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('amount');
+            $table->integer('fee_id');
+            $table->integer('student_id');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('transactions');
+    }
+}
