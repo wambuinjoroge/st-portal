1392362104b452cbd28785d613b48fdcb5d7ee88
wambuinjoroge <christinenjoroge93@gmail.com>
2017-02-06 11:28:50 -0500

    more

---
+++ app/Http/Controllers/FeesController.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Fees;
+use Illuminate\Http\Request;
+
+use App\Http\Requests;
+use Illuminate\Support\Facades\Validator;
+
+class FeesController extends Controller
+{
+    //
+    public function index(){
+      }
+    public function create(){
+       }
+
+    public function store(Request $request){
+       }
+    public function show(){
+
+    }
+    public function edit($id){
+
+    }
+    public function update(Request $request ,$id){
+
+    }
+    public function destroy($id){
+
+    }
+
+}
+
+
+
+
+
+
+
